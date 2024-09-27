b7e3515cfb6b7c5e9064aa479094faba1bf557a2
María Victoria Doll <mvictoriadoll@gmail.com>
2024-09-28 00:07:50 +0200

    add GET

--- src/index.js
+++ src/index.js
@@ -22,12 +22,42 @@ app.listen(port, () => {
       return;
     }
 
-    const [results] = await conn.query('SELECT * FROM PersonajesFriends;');
+    let selectStmt = 'SELECT * FROM friends.PersonajesFriends PF';
+    const values = [];
+
+    if ( req.query.incluir_actor && req.query.incluir_actor === 'true') {
+        selectStmt = selectStmt = `
+        SELECT PF.*, AF.nacionalidad 
+        FROM friends.PersonajesFriends PF 
+        JOIN friends.ActoresFriends AF ON (PF.idActoresFriends = AF.idActoresFriends)`;    
+    }
+
+    if( req.query.search ) {
+        selectStmt += " WHERE nombre LIKE ?"
+        values.push(`%${req.query.search}%`)
+      }    
+
+    const [results] = await conn.query(selectStmt, values);
 
     res.json(results);
 
     await conn.close();
   });
+
+  app.post ('api/friends', async (req,res) => {
+
+    if (!conn) {
+        res.status(500).json({ success: false, error: "Error con la conexion." });
+        return;
+      }
+
+      const [results] = await conn.execute (`
+        INSERT PersonajesFriends (nombre, actor, ocupación, edad)
+        VALUES ('Rachel Green', '', '','');`);
+
+      await conn.close();
+
+  })
 
   
     
