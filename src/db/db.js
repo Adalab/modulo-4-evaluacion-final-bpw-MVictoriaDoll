8e516baf17540c95fd1088eae4ead1f6b2a386ca
María Victoria Doll <mvictoriadoll@gmail.com>
2024-09-27 21:11:07 +0200

    add get 	# Please enter the commit message for your changes. Lines starting

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,9 @@
 
 require('dotenv').config();
 
+const mysql = require("mysql2/promise");
+
+
 async function getConnection () {
     try {
       const conn = await mysql.createConnection({
@@ -8,7 +11,7 @@ async function getConnection () {
         port: 3306,
         user: process.env['DB_USER'],
         password: process.env['DB_PASS'],
-        //database: process.env['DB_SCHEMA']
+        database: process.env['DB_SCHEMA']
       });
   
       await conn.connect();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 const mysql = require("mysql2/promise");
 
-const cors = require("cors");
+const getConnection = require('./db/db');
 
 const app = express();
 const port = 4000;
 
 app.use( cors() );
-app.use( express,json({limit: '25mb'}));
+app.use( express.json({limit: '25mb'}));
 
-const getConnection = require('./db/dbnpm');
+app.listen(port, () => {
+    console.log(`Servidor iniciado escuchando en http://localhost:${port}`);
+  });
+
+  app.get("/api/friends", async (req, res) => {
+    // Obtener conn con MySQL
+    const conn = await getConnection();
+    if (!conn) {
+      res.status(500).json({ success: false, error: "Error con la conexion." });
+      return;
+    }
+
+    const [results] = await conn.query('SELECT * FROM PersonajesFriends;');
+
+    res.json(results);
+
+    await conn.close();
+  });
+
+  
+    
